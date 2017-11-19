7a8a7cea145f87d1f9c264d3af005749fce535a3
Michael Jordan <michaeljordan@Michaels-MacBook-Pro.local>
2017-11-19 15:40:03 -0800

    added tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@progress/kendo-angular-layout'
 
 
 import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]

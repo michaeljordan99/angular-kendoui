0120d170564f36e69930d38891093b7c800bbda3
Michael Jordan <michaeljordan@Michaels-MacBook-Pro.local>
2017-11-19 16:42:02 -0800

    added products and master-detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LayoutModule } from '@progress/kendo-angular-layout'
+import { HttpClientModule } from '@angular/common/http';
 
+import { LayoutModule } from '@progress/kendo-angular-layout'
+import { GridModule } from '@progress/kendo-angular-grid';
 
 import { AppComponent } from './app.component';
 import { TabsComponent } from './tabs/tabs.component';
-
+import { ProductsGridComponent } from './products/products-grid.component'
+import { MasterDetailGridComponent } from './master-detail/master-detail.component'
+import { CategoryDetailComponent } from './master-detail/category-details.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    TabsComponent
+    TabsComponent,
+    ProductsGridComponent,
+    MasterDetailGridComponent,
+    CategoryDetailComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    LayoutModule
+    LayoutModule,
+    GridModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
